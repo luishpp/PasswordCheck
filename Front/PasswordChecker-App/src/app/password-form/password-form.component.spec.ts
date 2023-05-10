985eb25fdd2525e67a47d6c5ebe0e287897c934b
Luis Henrique <luishpp@hotmail.com>
2023-05-10 10:33:26 -0300

    Integration tests adjustments

--- Front/PasswordChecker-App/src/app/password-form/password-form.component.spec.ts
+++ Front/PasswordChecker-App/src/app/password-form/password-form.component.spec.ts
@@ -22,32 +22,41 @@ describe('PasswordFormComponent', () => {
   });
 
   it('should display "Valid password" when a valid password is entered', () => {
-    const inputEl = fixture.nativeElement.querySelector('#password');
-    inputEl.value = 'AbTp9!fok';
-    inputEl.dispatchEvent(new Event('input'));
+    const inputElement = fixture.nativeElement.querySelector('#password');
+    inputElement.value = 'AbTp9!fok';
+    inputElement.dispatchEvent(new Event('input'));
 
-    const buttonEl = fixture.nativeElement.querySelector('button');
-    buttonEl.click();
+    const buttonElement = fixture.nativeElement.querySelector('button');
+    buttonElement.click();
     fixture.detectChanges();
 
-    const resultEl = fixture.nativeElement.querySelector('p');
-    if (resultEl != null){
-      expect(resultEl.textContent).toContain('Valid password');
+    const resultElelent = fixture.nativeElement.querySelector('[data-id="result"]');
+    if (resultElelent != null){
+      expect(resultElelent.textContent).toContain('Valid password');
     }
+
+    //console.log(inputElement.value);
+    //console.log(buttonElement);
+    //console.log(resultElelent);
   });
 
   it('should display "Invalid password" when an invalid password is entered', () => {
-    const inputEl = fixture.nativeElement.querySelector('#password');
-    inputEl.value = 'invalidpassword';
-    inputEl.dispatchEvent(new Event('input'));
+    const inputElement = fixture.nativeElement.querySelector('#password');
+    inputElement.value = 'invalidpassword';
+    inputElement.dispatchEvent(new Event('input'));
 
-    const buttonEl = fixture.nativeElement.querySelector('button');
-    buttonEl.click();
+    const buttonElement = fixture.nativeElement.querySelector('button');
+    buttonElement.click();
     fixture.detectChanges();
 
-    const resultEl = fixture.nativeElement.querySelector('p');
-    if (resultEl != null){
-      expect(resultEl.textContent).toContain('Invalid password');
+    const resultElelent = fixture.nativeElement.querySelector('[data-id="result"]');
+    if (resultElelent != null){
+      expect(resultElelent.textContent).toContain('Invalid password');
     }
+
+    //console.log(inputElement.value);
+    //console.log(buttonElement);
+    //console.log(resultElelent);
   });
 });
+
